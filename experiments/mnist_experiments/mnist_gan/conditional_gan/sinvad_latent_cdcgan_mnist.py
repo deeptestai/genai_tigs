1f6dca2f689333887487e3ce638ecbe847082dde
Maryam <129972846+Maryammaryam877@users.noreply.github.com>
2024-04-24 13:35:33 +0200

    Update sinvad_latent_cdcgan_mnist.py

--- experiments/mnist_experiments/mnist_gan/conditional_gan/sinvad_latent_cdcgan_mnist.py
+++ experiments/mnist_experiments/mnist_gan/conditional_gan/sinvad_latent_cdcgan_mnist.py
@@ -52,16 +52,22 @@ def calculate_fitness(logit, label):
     # Compute fitness value
     fitness = expected_logit - new_logit
     return fitness
-
+# Directory to save images for analysis
+image_save_dir = './generated_images'
+os.makedirs(image_save_dir, exist_ok=True)
 
 image_info = []
 predictions = []
 num_samples = 100
-gen_num = 500
+gen_num = 250
 pop_size = 25
 best_left = 10
-perturbation_size = 0.02  # Default perturbation size
-initial_perturbation_size = 0.01  # Initial perturbation size
+min_val = -4.78276300430298
+max_val = 4.08758640289307
+
+
+perturbation_size = 0.00887034940719605  # Default perturbation size
+initial_perturbation_size = 0.0177406988143921 # Initial perturbation size
 # Generate a random latent vector
 latent_space = torch.randn(num_samples, 100, 1, 1).to(device)
 random_labels = torch.randint(0, 10, (num_samples,)).to(device)
@@ -74,22 +80,25 @@ for i in range(num_samples):
     original_image = G(original_latent.view(1, 100, 1, 1), original_labelr.view(1))
     original_logit = classifier(original_image).squeeze().detach().cpu().numpy()
     original_label = np.argmax(original_logit).item()
-    # Convert the tensor to a PIL Image
-    original_image_pil = transforms.ToPILImage()(original_image.squeeze().cpu())
-
-    # Define the path for saving the original image
-    # You can include additional identifiers in the filename if needed, such as a timestamp or iteration number
-    original_image_path = os.path.join(
-        original_images_dir, f"original_image_{i}_X{original_label}.png"
-    )
-
+    
     # Save the original image
     original_image_pil.save(original_image_path)
+    # Convert the tensor to a NumPy array
+    original_image_np = original_image.squeeze().detach().cpu().numpy()
+
+    # Define the filename that includes the label
+    filename = f"original_image_{i}_X_{original_label}.npy"
+
+    # Define the full path for saving the file
+    file_path = os.path.join(original_images_dir, filename)
+
+    # Save the image data as a NumPy file
+    np.save(file_path, original_image_np)
 
     ### Initialize optimization ###
     init_pop = [
-        original_latent.unsqueeze(0)
-        + initial_perturbation_size * torch.randn_like(original_latent)
+            original_latent.unsqueeze(0)
+            + initial_perturbation_size * torch.randn_like(original_latent)
         for _ in range(pop_size)
     ]
 
@@ -98,35 +107,50 @@ for i in range(num_samples):
     binom_sampler = torch.distributions.binomial.Binomial(
         probs=0.5 * torch.ones(original_latent.size(), device=device)
     )
-    init_label = torch.tensor([original_label] * pop_size)
+    best_fitness_score = np.inf
+    best_image_tensor = None
+    best_image_index = -1
+    init_label = [original_label] * (pop_size)
     ###GA_algorithm###
     for g_idx in range(gen_num):
         # Combine current latent vectors and labels
-        indivs_latent = torch.cat(now_pop, dim=0).view(-1, 100, 1, 1)
-        indivs_labels = init_label
+        indivs_latent = torch.cat(now_pop, dim=0).view(-1, 100, 1, 1).to(device)
+        indivs_labels = torch.tensor(init_label).to(device)
 
         # Generate images using the generator
         Gen_imgs = G(indivs_latent, indivs_labels)
         # Gen_imgs1 = Gen_imgs.detach().cpu().numpy()
-        predicted_labels = torch.argmax(classifier(Gen_imgs), dim=1).cpu()
+        predicted_labels = torch.argmax(classifier(Gen_imgs), dim=1).to(device)
         # all_img_lst1.append(Gen_imgs1)
         all_logits = classifier(Gen_imgs).squeeze().detach().cpu().numpy()
         fitness_scores = [
             calculate_fitness(all_logits[k_idx], original_label)
             for k_idx in range(pop_size)
         ]
+        # Finding the minimum fitness score in this generation
+        current_min_index = np.argmin(fitness_scores)
+        current_min_fitness = fitness_scores[current_min_index]
+
+        # Update global minimum if the current score is lower
+        if current_min_fitness < best_fitness_score:
+            best_fitness_score = current_min_fitness
+            best_image_tensor = Gen_imgs[current_min_index].cpu().detach()
+            best_image_index = current_min_index
         # Selection
         selected_indices = sorted(
             range(len(fitness_scores)), key=lambda i: fitness_scores[i], reverse=True
         )[-best_left:]
+        
+        # Correctly finding the minimum fitness score among selected individuals
+       # selected_fitness_scores = [fitness_scores[i] for i in selected_indices]
         now_best = np.min(fitness_scores)
 
         parent_pop = [now_pop[i] for i in selected_indices]
-
         # Perform crossover and mutation
         print("now_best " + str(now_best) + " now_best " + str(np.mean(fitness_scores)))
         # wandb.log({"ft_scores": now_best})
 
+        # Check condition to save image
         if now_best < 0:
             break
         elif now_best == prev_best:
@@ -134,8 +158,9 @@ for i in range(num_samples):
         else:
             perturbation_size = initial_perturbation_size
 
-        # Crossover and Mutation for both latent vectors and labels
+        # Crossover and Mutation for latent vectors
         k_pop = []
+        k_labels = []  # Initialize to store new labels
         for k_idx in range(pop_size - best_left):
             mom_idx, pop_idx = np.random.choice(best_left, size=2, replace=False)
             spl_idx = np.random.choice(100, size=1)[0]
@@ -146,58 +171,31 @@ for i in range(num_samples):
 
             # Mutation for latent vector
             diffs = (k_gene != original_latent).float()
-            k_gene += perturbation_size * torch.randn_like(k_gene) * diffs
-
+            k_gene += perturbation_size * torch.randn_like(k_gene).to(device) * diffs
             # Perform random matching to latent_images[i]
-            interp_mask = binom_sampler.sample()
-            k_gene = interp_mask * original_latent + (1 - interp_mask) * k_gene
-
+           # interp_mask = binom_sampler.sample().to(device)
+           # k_gene = interp_mask * original_latent + (1 - interp_mask) * k_gene
             k_pop.append(k_gene)
 
         now_pop = parent_pop + k_pop
         prev_best = now_best
-        init_label = predicted_labels
+        now_pop = [torch.clamp(tensor, min=min_val, max=max_val) for tensor in now_pop]
 
-    mod_best_latent = parent_pop[-1].view(-1, 100, 1, 1)
-    # Adjusted expansion
-    mod_best_image = G(mod_best_latent, random_labels[i].unsqueeze(0))
-    # Convert the tensor to a PIL Image
-    transform = transforms.ToPILImage()
-    perturbed_img_pil = transform(mod_best_image[0].detach().cpu())
+    mod_best_image_tensor = best_image_tensor.to(device)
+    mod_best_image_np = best_image_tensor.cpu().detach().numpy()
 
-    final_bound_logits = classifier(mod_best_image)
-    mod_best_label = torch.argmax(final_bound_logits, dim=1).item()
-
-    all_img_lst.append(perturbed_img_pil)
-
-    image_path = os.path.join(
+    final_bound_logits = classifier(mod_best_image_tensor.unsqueeze(0))
+    predicted_best_label = torch.argmax(final_bound_logits, dim=1).item()
+     # Define the path for saving the numpy file with detailed filename
+    image_path_np = os.path.join(
         result_dir,
-        f"image_{i}_iteration{g_idx}_X{original_label}_Y{mod_best_label}.png",
+        f"image_{i}_iteration{g_idx}_X{original_label}_Y{predicted_best_label}.npy"
     )
-    # Save the image directly without converting to a PIL Image first
-    perturbed_img_pil.save(image_path)
 
-    # Store the image info
-    image_info.append((i, g_idx, original_label, mod_best_label))
+    # Save the numpy array to a file
+    np.save(image_path_np, mod_best_image_np)
+    all_img_lst.append(mod_best_image_np)
 
 # Save the images as a numpy array
 all_imgs = np.vstack(all_img_lst)
 np.save(os.path.join(result_dir, "bound_imgs_mnist_cdcgan.npy"), all_imgs)
-
-# Save the image info
-with open(os.path.join(result_dir, "image_info.txt"), "w") as f:
-    f.write("Image Index, Expected Label X, Predicted Label Y\n")
-    for img_info in image_info:
-        f.write(f"{img_info[0]}, {img_info[1]}, {img_info[2]}, {img_info[3]}\n")
-misclassified_count = 0
-
-# Iterate over the image info list
-for img_info in image_info:
-    expected_label = img_info[2]
-    predicted_label = img_info[3]
-    if predicted_label != expected_label:
-        misclassified_count += 1
-
-misclassification_percentage = (misclassified_count / len(image_info)) * 100
-
-print(f"Misclassification Percentage: {misclassification_percentage:.2f}%")
