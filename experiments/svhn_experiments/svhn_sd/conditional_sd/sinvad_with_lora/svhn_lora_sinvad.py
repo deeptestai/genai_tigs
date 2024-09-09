696355a4e4fcb8b8aec5fa7be337ac21fbb0c184
Maryam <129972846+Maryammaryam877@users.noreply.github.com>
2024-09-09 16:15:15 +0200

    Update svhn_lora_sinvad.py

--- experiments/svhn_experiments/svhn_sd/conditional_sd/sinvad_with_lora/svhn_lora_sinvad.py
+++ experiments/svhn_experiments/svhn_sd/conditional_sd/sinvad_with_lora/svhn_lora_sinvad.py
@@ -5,18 +5,16 @@ import cv2
 from diffusers import  StableDiffusionPipeline
 from DeepCache import DeepCacheSDHelper
 from tgate import TgateSDDeepCacheLoader
-#from StableDiffusionParallPipeline.libs.StableDiffusionParallelPipeline import StableDiffusionParallelPipeline
 from diffusers.schedulers import DPMSolverMultistepScheduler
-#from libs.benchmark import benchmark
 import numpy as np
 import torch
-import wandb
+#import wandb
 from torch import autocast
 from PIL import Image
 from torchvision import transforms
 from collections import Counter
 from svhn_classifier.model import VGGNet
-run =wandb.init(project="sinvadtestfitness")
+#run =wandb.init(project="sinvadtestfitness")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
 torch.backends.cudnn.benchmark = False
@@ -95,16 +93,16 @@ best_image_index = -1
 init_perturbation = 0.00217826347351074
 best_left = 10
 perturbation_size = 0.00108913173675537
-gen_steps = 250
+gen_num = 250
 pop_size = 25
 min_val = -5.62070274353027
 max_val = 5.27061462402344
 fitness_scores = []
 all_img_lst = []
-num_samples = 100
+imgs_to_samp = 10
 image_info = []
 predicted_labels = []
-proj_path = "./result_svhnup_sdrun0gs3.5,1.5/"+proj_name+"_"
+proj_path = "./svhn_sd/"+proj_name+"_"
 os.makedirs(proj_path, exist_ok=True)
 os.makedirs(proj_path+'/Newresult', exist_ok=True)
 print('Creating init image')
@@ -115,11 +113,9 @@ weights_path = "./svhn_finetune_lorav1.5-000005.safetensors"
 pipe = StableDiffusionPipeline.from_pretrained(
 base_model_id,variant="fp16", torch_dtype=torch.float16, safety_checker=None).to(device)
 pipe.load_lora_weights(weights_path)
-#vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae", use_safetensors=True)
-#pipe.vae = AutoencoderTiny.from_pretrained("madebyollin/taesd", torch_dtype=torch.float16)
 pipe = pipe.to(device)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(
-    pipe.scheduler.config)                            #rescale_betas_zero_snr=True)
+    pipe.scheduler.config)                            
 pipe.unet.to(device)
 pipe.vae.to(device)
 pipe.text_encoder.to(device)
@@ -129,201 +125,154 @@ pipe = TgateSDDeepCacheLoader(
        cache_branch_id=0,
 ).to(device)
 seed =0
-for n in range(num_samples):
+saved_images = 0
+for n in range(imgs_to_samp):
     seedSelect = seed+n
     generator = generator.manual_seed(seedSelect)
     original_lv = torch.randn((1, pipe.unet.config.in_channels, height // 8, width // 8),generator = generator, device=device).to(torch.float16)
-    label_matched = False  # Flag to track when the correct label is matched
-    attempt_count = 0  # To prevent infinite loops
 
-    while not label_matched and attempt_count < 10:
-         randprompt = random.choice(prompts)
-         expected_label = int(re.search(r"HouseNo(\d+)", randprompt).group(1))  # Extract the number following 'HouseNo'
-         with torch.inference_mode():
-              init_img = pipe.tgate(prompt = randprompt,guidance_scale= 3.5,gate_step =10, num_inference_steps= num_inference_steps,latents=original_lv)["images"][0]
-         tensor_image = process_image(init_img)
-         tensor_image = tensor_image.unsqueeze(0).to(device)
-         original_logit = classifier(tensor_image).squeeze().detach().cpu().numpy()
-         original_label = np.argmax(original_logit).item()
-         # Check if the predicted label matches the expected label from the prompt
-         if original_label == expected_label:
-           # Save the image only if the label matches
-            init_img_path = os.path.join(proj_path, f'image_{n}_X{original_label}_prompt_{expected_label}.png')
-            init_img.save(init_img_path)
-            print(f"Image {n} with matching label saved at {init_img_path}")
-            label_matched = True
-         else:
-            print(f"Attempt {attempt_count + 1}: Label mismatch for image {n}: expected {expected_label}, got {original_label}")
-            attempt_count += 1
-    if not label_matched:
-        print(f"No matching prompt found after 10 attempts for image {n}.")    
+    # Generate Prompt and expected label
+    randprompt = random.choice(prompts)
+    expected_label = int(re.search(r"HouseNo(\d+)", randprompt).group(1))  # Extract the number following 'HouseNo'
+    with torch.inference_mode():
+        init_img = pipe.tgate(prompt = randprompt,guidance_scale= 3.5,gate_step =10, num_inference_steps= num_inference_steps,latents=original_lv)["images"][0]
+    #process the generated image
+    tensor_image = process_image(init_img)
+    tensor_image = tensor_image.unsqueeze(0).to(device)
+    original_logit = classifier(tensor_image).squeeze().detach().cpu().numpy()
+    original_label = np.argmax(original_logit).item()
+    # Check if the predicted label matches the expected label from the prompt
+    if original_label == expected_label:
+       # Save the image only if the label matches
+       init_img_path = os.path.join(proj_path, f'image_{saved_images}_X{original_label}_prompt_{expected_label}.png')
+       init_img.save(init_img_path)
+       print(f"Image {saved_images} with matching label saved at {init_img_path}")
+       saved_images +=1
 
-    init_pop = [
-      original_lv + init_perturbation * torch.randn((pipe.unet.config.in_channels, height // 8, width // 8), device=device).to(torch.float16)
-       for _ in range(pop_size)
-    ]
+       init_pop = [
+         original_lv + init_perturbation * torch.randn((pipe.unet.config.in_channels, height // 8, width // 8), device=device).to(torch.float16)
+         for _ in range(pop_size)
+       ]
 
-    best_fitness_score = float('inf')
-    best_image_tensor = None
-    best_image_index = -1
-    now_pop = init_pop
-    prev_best = np.inf
-    for i in range(gen_steps):
-       # Flatten all tensors in now_pop into a single tensor for min/max evaluation
-        indivs_lv = torch.cat(now_pop, dim=0).view(-1, 4, height // 8, width // 8).to(torch.float16)
-        print(indivs_lv.shape)
-        #per
-        with torch.inference_mode():                         # torch.no_grad(),torch.cuda.amp.autocast(dtype =torch.bfloat16):
-             perturb_img = pipe.tgate([randprompt]*(pop_size),guidance_scale =1.4,gate_step = 10, generator= generator, 
-                num_inference_steps=num_inference_steps,
-                latents=indivs_lv,
-             )["images"]
-        # all_img_lst.append(perturb_img)
-        torch.cuda.empty_cache()
+       best_fitness_score = float('inf')
+       best_image_tensor = None
+       best_image_index = -1
+       now_pop = init_pop
+       prev_best = np.inf
+       for g_idx in range(gen_num):
+          # Flatten all tensors in now_pop into a single tensor for min/max evaluation
+           indivs_lv = torch.cat(now_pop, dim=0).view(-1, 4, height // 8, width // 8).to(torch.float16)
+           print(indivs_lv.shape)
+           
+           with torch.inference_mode():                         
+                perturb_img = pipe.tgate([randprompt]*(pop_size),guidance_scale =1.4,gate_step = 10, generator= generator, 
+                   num_inference_steps=num_inference_steps,
+                   latents=indivs_lv,
+                )["images"]
+           # all_img_lst.append(perturb_img)
+           torch.cuda.empty_cache()
 
-       # model_image_pil = Image.fromarray(np.uint8(last_image))
-       # resized_image2 = model_image_pil.resize((28, 28), Image.Resampling.LANCZOS)
-        tensor_image2 =torch.stack([process_image(image) for image in perturb_img])
-       # tensor_image2 = transform(last_image)
-        tensor_image2 = tensor_image2.to(device)
-        all_logits = classifier(tensor_image2).detach().cpu().numpy()
-        perturb_label1 = np.argmax(all_logits).item()
-        print(all_logits.shape)
-        print(tensor_image2.shape)
-        os.makedirs(os.path.join(proj_path, 'generated_images'), exist_ok=True)
-       # label_filenames = []
+           tensor_image2 =torch.stack([process_image(image) for image in perturb_img])
+           # tensor_image2 = transform(last_image)
+           tensor_image2 = tensor_image2.to(device)
+           all_logits = classifier(tensor_image2).detach().cpu().numpy()
+           perturb_label1 = np.argmax(all_logits).item()
+           print(all_logits.shape)
+           print(tensor_image2.shape)
+           os.makedirs(os.path.join(proj_path, 'generated_images'), exist_ok=True)
+          
+           fitness_scores = [
+               calculate_fitness(all_logits[k_idx], original_label)
+               for k_idx in range(pop_size)
+           ]
+           print("print fitness",len(fitness_scores))
 
-        # Loop through the logits and save each image with its label
-       # for idx, logits in enumerate(all_logits):
-            # label = np.argmax(logits)
-           # Get the image from perturb_img using idx
-            # image = perturb_img[idx]
-            # perturb_label_filename = f'gen_{i}_image_{idx}_Y{label}.png'
+           # Find the minimum fitness score in the current generation
+           current_min_index = np.argmin(fitness_scores)
+           current_min_fitness = fitness_scores[current_min_index]
     
-           # Save the image with the label in the filename
-            # image_filename = os.path.join(proj_path, 'generated_images', perturb_label_filename)
-            # image.save(image_filename)
-    
-            # Append the label and filename to the list
-            # label_filenames.append((label, perturb_label_filename))
+           # Update best tracking variables if the current minimum is less than the tracked best
+           if current_min_fitness < best_fitness_score:
+              best_fitness_score = current_min_fitness
+              best_image_tensor  = tensor_image2[current_min_index]  # Ensure a deep copy
+              best_image_index = current_min_index
+           # Perform selection
+           selected_indices = sorted(range(len(fitness_scores)), key=lambda i: fitness_scores[i], reverse=True,
+              )[-best_left:]
+           now_best = np.min(fitness_scores)
+           parent_pop = [now_pop[idx] for idx in selected_indices]
+           print(parent_pop[-1].shape)
+           print("now_best " + str(now_best) + " average_best " + str(np.mean(fitness_scores)))
+          # wandb.log({"ft_score":now_best})
+           if now_best < 0:
+              break
+           elif now_best == prev_best:
+               perturbation_size *= 2
+           else:
+               perturbation_size = init_perturbation
+           k_pop = []
+           # print("Size of parent_pop:", len(parent_pop))
+           # select k-idx for cross_over genes
+           for k_idx in range(pop_size - best_left):
+               mom_idx, pop_idx = np.random.choice(best_left, size=2, replace=False)
+              # print("mom_idx:", mom_idx, "pop_idx:", pop_idx)
+               spl_idx = np.random.choice(4, size=1)[0]
+               k_gene = torch.cat(
+                   [parent_pop[mom_idx][:, :spl_idx], parent_pop[pop_idx][:, spl_idx:]],
+                   dim=1,
+               )  # crossover
+               # Mutation
+               diffs = (k_gene != original_lv).float()
+               k_gene += (
+                      perturbation_size * torch.randn(k_gene.size(), device=k_gene.device) * diffs
+               )  # random adding noise only to diff places
+              # k_gene = torch.clamp(k_gene, min_val, max_val)
+              # interp_mask = binom_sampler.sample()
+              # k_gene = interp_mask * original_lv + (1 - interp_mask) * k_gene
+               k_pop.append(k_gene)
+               # print("Size of k_pop:", len(k_pop))
 
-        # Save the label filenames to a text file
-       # label_file = os.path.join(proj_path, 'generated_images', 'label_filenames.txt')
-       # with open(label_file, 'w') as file:
-           # for label, filename in label_filenames:
-               # file.write(f"Label: {label}, Filename: {filename}\n")
-
-        # Count the label occurrences and print them
-       # label_list = [label for label, _ in label_filenames]
-        #3label_counts = Counter(label_list)
-       # for label, count in label_counts.items():
-            # print(f"Label {label}: Count {count}")
-        fitness_scores = [
-            calculate_fitness(all_logits[k_idx], original_label)
-            for k_idx in range(pop_size)
-        ]
-        print("print fitness",len(fitness_scores))
-
-        # Find the minimum fitness score in the current generation
-        current_min_index = np.argmin(fitness_scores)
-        current_min_fitness = fitness_scores[current_min_index]
-    
-        # Update best tracking variables if the current minimum is less than the tracked best
-        if current_min_fitness < best_fitness_score:
-           best_fitness_score = current_min_fitness
-           best_image_tensor  = tensor_image2[current_min_index]  # Ensure a deep copy
-           best_image_index = current_min_index
-        # Perform selection
-        selected_indices = sorted(range(len(fitness_scores)), key=lambda i: fitness_scores[i], reverse=True,
-           )[-best_left:]
-        now_best = np.min(fitness_scores)
-        parent_pop = [now_pop[idx] for idx in selected_indices]
-        print(parent_pop[-1].shape)
-        print("now_best " + str(now_best) + " average_best " + str(np.mean(fitness_scores)))
-        wandb.log({"ft_score":now_best})
-        if now_best < 0:
-           break
-        elif now_best == prev_best:
-            perturbation_size *= 2
-        else:
-            perturbation_size = init_perturbation
-        k_pop = []
-       # print("Size of parent_pop:", len(parent_pop))
-        # select k-idx for cross_over genes
-        for k_idx in range(pop_size - best_left):
-            mom_idx, pop_idx = np.random.choice(best_left, size=2, replace=False)
-           # print("mom_idx:", mom_idx, "pop_idx:", pop_idx)
-            spl_idx = np.random.choice(4, size=1)[0]
-            k_gene = torch.cat(
-                [parent_pop[mom_idx][:, :spl_idx], parent_pop[pop_idx][:, spl_idx:]],
-                dim=1,
-            )  # crossover
-            # Mutation
-            diffs = (k_gene != original_lv).float()
-            k_gene += (
-                   perturbation_size * torch.randn(k_gene.size(), device=k_gene.device) * diffs
-            )  # random adding noise only to diff places
-           # k_gene = torch.clamp(k_gene, min_val, max_val)
-           # interp_mask = binom_sampler.sample()
-           # k_gene = interp_mask * original_lv + (1 - interp_mask) * k_gene
-            k_pop.append(k_gene)
-           # print("Size of k_pop:", len(k_pop))
-
-        # Combine parent_pop and k_pop for the next generation
-        now_pop = parent_pop + k_pop
-        prev_best = now_best
-        # Apply a final clamp across all now_pop before next iteration starts
-        now_pop = [torch.clamp(tensor, min=min_val, max=max_val) for tensor in now_pop]
+           # Combine parent_pop and k_pop for the next generation
+           now_pop = parent_pop + k_pop
+           prev_best = now_best
+           # Apply a final clamp across all now_pop before next iteration starts
+           now_pop = [torch.clamp(tensor, min=min_val, max=max_val) for tensor in now_pop]
       
 
-   # mod      # .view(1, pipe.unet.config.in_channels, height // 8, width // 8).to(torch.float1
-   # with torch.inference_mode():
+      # mod      # .view(1, pipe.unet.config.in_channels, height // 8, width // 8).to(torch.float1
+      # with torch.inference_mode():
 
       # last_image_list = pipe.tgate(prompt=randprompt,guidance_scale= 1.4, gate_step = 10,  num_inference_steps= 10, latents=mod_best)["images"]
-    # After the loop, save the last image if it exists
-    if best_image_tensor is not None:
-       tensor_image = best_image_tensor.to(device)
-       tensor_image = tensor_image.unsqueeze(0).to(device)
-       tensor_image_np= tensor_image.squeeze().detach().cpu().numpy()
-       perturb_logit = classifier(tensor_image).squeeze().detach().cpu().numpy()
-       perturb_label = np.argmax(perturb_logit).item()
-       predicted_labels.append(perturb_label)
-       all_img_lst.append(tensor_image_np)
-       # Save the image as a numpy array
-       image_filename = f'image_{n}_iteration{i}_X{original_label}_Y{perturb_label}.png'
-       # Save the image as a numpy array
-       np.save(os.path.join(proj_path,'generated_images', image_filename), tensor_image_np)
+     # After the loop, save the last image if it exists
+       if best_image_tensor is not None:
+          tensor_image = best_image_tensor.to(device)
+          tensor_image = tensor_image.unsqueeze(0).to(device)
+          tensor_image_np= tensor_image.squeeze().detach().cpu().numpy()
+          perturb_logit = classifier(tensor_image).squeeze().detach().cpu().numpy()
+          perturb_label = np.argmax(perturb_logit).item()
+          predicted_labels.append(perturb_label)
+          all_img_lst.append(tensor_image_np)
+          # Save the image as a numpy array
+          image_filename = f'image_{saved_images}_iteration{g_idx + 1}_X{original_label}_Y{perturb_label}'
+          # Save the image as a numpy array
+          np.save(os.path.join(proj_path,'generated_images', image_filename), tensor_image_np)
+       else: 
+          print("image is none")
     else: 
-       print("image is none")
+       # IF no match, simply skip to the next iteration (no image is saved or processed)
+       print(f"Label mismatch for image {saved_images}:expected {expected_label},got {original_label}")
     #For saving image for myself
-    if best_image_tensor is not None:
+   # if best_image_tensor is not None:
        # Ensure tensor is on CPU and squeeze out the batch dimension if necessary
-       if best_image_tensor.dim() == 4 and best_image_tensor.shape[0] == 1:  # Checking for single item in batch
-            tensor_image = best_image_tensor.squeeze(0).cpu()
-       else:
-            tensor_image = best_image_tensor.cpu()
-    best_image_pil = transforms.ToPILImage()(tensor_image.cpu())
-    image_filename = f'image_{n}_iteration{i}_X{original_label}_Y{perturb_label}.png'
-    last_image_path = os.path.join(proj_path, 'Newresult', image_filename)
-    best_image_pil.save(last_image_path)
-    image_info.append((n, i, original_label, perturb_label))
+      # if best_image_tensor.dim() == 4 and best_image_tensor.shape[0] == 1:  # Checking for single item in batch
+       #     tensor_image = best_image_tensor.squeeze(0).cpu()
+      # else:
+       #     tensor_image = best_image_tensor.cpu()
+  #  best_image_pil = transforms.ToPILImage()(tensor_image.cpu())
+   # image_filename = f'image_{n}_iteration{i}_X{original_label}_Y{perturb_label}.png'
+   # last_image_path = os.path.join(proj_path, 'Newresult', image_filename)
+   # best_image_pil.save(last_image_path)
+   
 # Save the images as a numpy array
 all_imgs = np.vstack(all_img_lst)
 np.save(os.path.join(proj_path, "bound_imgs_svhn_sd.npy"), all_imgs)
-# Save the image info
-with open(os.path.join(proj_path, "image_info.txt"), "w") as f:
-    f.write("Image Index,Iteration,Expected Label X, Predicted Label Y\n")
-    for img_info in image_info:
-        f.write(f"{img_info[0]}, {img_info[1]}, {img_info[2]}, {img_info[3]}\n")
-misclassified_count = 0
-
-# Iterate over the image info list
-for img_info in image_info:
-    expected_label = img_info[2]
-    predicted_label = img_info[3]
-    if predicted_label != expected_label:
-        misclassified_count += 1
-
-misclassification_percentage = (misclassified_count / len(image_info)) * 100
-
-print(f"Misclassification Percentage: {misclassification_percentage:.2f}%")
