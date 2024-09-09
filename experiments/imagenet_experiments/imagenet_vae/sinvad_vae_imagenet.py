c560f1d1ddc1675a2efb9e24d77ba0c04f587da9
Maryam <129972846+Maryammaryam877@users.noreply.github.com>
2024-09-09 15:03:24 +0200

    Update sinvad_vae_imagenet.py

--- experiments/imagenet_experiments/imagenet_vae/sinvad_vae_imagenet.py
+++ experiments/imagenet_experiments/imagenet_vae/sinvad_vae_imagenet.py
@@ -1,83 +1,63 @@
 import torch
 import os
-import json
-from torchvision.transforms import InterpolationMode
-from PIL import Image, ImageFilter
-import wandb
-import numpy as np  #### working on it
-from PIL import Image
-import matplotlib.pyplot as plt
-import torchvision
-from torchvision import transforms
-import torchvision.transforms.functional as F
-from tqdm import trange
-import torchvision.utils as vutils
-from torchvision import datasets, transforms
+import numpy as np
+from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
-from vae_model import  VAE, Encoder,  Decoder
-
-
+from vae_model_traink import Model
+import torchvision.utils as vutils
+from PIL import Image
+from tqdm import trange
 def save_image(tensor, filename):
-    """
-    Save a tensor as an image
-    """
-    img = vutils.make_grid(tensor, normalize=True)  # Normalize ensures [0,1] range
+    img = vutils.make_grid(tensor, normalize=True)
     img = img.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0)
-    # Now, transfer the tensor to CPU and convert it to numpy array for saving
-    img = img.to(
-        "cpu", torch.uint8
-    ).numpy()  # Convert tensor to numpy array in [0,255] range
+    img = img.to("cpu", torch.uint8).numpy()
     img = Image.fromarray(img)
     img.save(filename)
+    print(f"Saved image: {filename}")
 
+def load_model(checkpoint_path):
+    model = Model(latent_dim=latent_dim).to(device)
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    return model
 
-# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# Model parameters
-latent_dim = 1024
+latent_dim = 512
 img_dim = 128
 img_chn = 3
 
-# Initialize the VAE model
-encoder = Encoder(img_dim, img_chn, latent_dim)
-decoder = Decoder(latent_dim, img_dim)
-vae_model = VAE(encoder, decoder).to(device)
-encoder.load_state_dict(
-    torch.load(
-        "./checkpoints/encoder_epoch_60.pth",
-        map_location=device,
-    )
-)
-decoder.load_state_dict(
-    torch.load(
-        "./checkpoints/decoder_epoch_60.pth",
-        map_location=device,
-    )
-)
-# Load pretrained model
-classifier = torch.hub.load("pytorch/vision:v0.10.0", "vgg19_bn", pretrained=True).to(
-    device
-)
-classifier.eval()
-vae_model.eval()  # Set the model to evaluation mode
-result_dir = "./result_vae_Imagenetsd"
-os.makedirs(result_dir, exist_ok=True)
-# Dataset and DataLoader setup
-VAL_DIR = "/home/vincenzo.riccio/ddpm_diffuser/validation_dataset"  # Update this path
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(),  # Convert the image to a tensor
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize the tensor
-    ]
-)
-val_dataset = datasets.ImageFolder(root=VAL_DIR, transform=transform)
-val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True)
-print("Data loader ready...")
+vae = Model(latent_dim=latent_dim).to(device)
+checkpoint_path = "./imagenet_vae_model_epoch_41.pth"
+vae = load_model(checkpoint_path)
+vae.eval()
 
+classifier = torch.hub.load("pytorch/vision:v0.10.0", "vgg16_bn", pretrained=True).to(device)
+classifier.eval()
+
+transform = transforms.Compose([
+    transforms.Resize(256, interpolation=transforms.InterpolationMode.BILINEAR, antialias=True),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Resize((128, 128))
+])
+
+result_dir = "./Imagenet_img"
+original_images_dir = os.path.join(result_dir, "original_images")
+os.makedirs(result_dir, exist_ok=True)
+os.makedirs(original_images_dir, exist_ok=True)
+img_pil_dir = os.path.join(original_images_dir, "or_images")
+os.makedirs(img_pil_dir, exist_ok=True)
+prtb_img_dir = os.path.join(result_dir, "perturb_images")
+os.makedirs(prtb_img_dir, exist_ok=True)
+
+TEST_DIR = "./test1_pizza"
+test_dataset = datasets.ImageFolder(root=TEST_DIR, transform=transform)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
+print("Data loader ready...")
 
 def calculate_fitness(logit, label):
     expected_logit = logit[label]
-    # Select the two best indices [perturbed logit]
     best_indices = np.argsort(-logit)[:2]
     best_index1, best_index2 = best_indices
     if best_index1 == label:
@@ -85,155 +65,152 @@ def calculate_fitness(logit, label):
     else:
         best_but_not_expected = best_index1
     new_logit = logit[best_but_not_expected]
-    # Compute fitness value
     fitness = expected_logit - new_logit
     return fitness
 
-
 image_info = []
-predictions = []
-gen_num = 500
+gen_num = 250
 pop_size = 25
 best_left = 10
-num_samples = 8
-perturbation_size = 0.5  # Default perturbation size
-initial_perturbation_size = 0.1  # Initial perturbation size
-#latent_space = torch.randn(num_samples, 1024, 1, 1).to(device)
+min_val = -5.22431135177612
+max_val = 5.62481260299683
+imgs_to_samp = 100
+Expected_class_index = 963
+perturbation_size = 0.0010849123954773
+initial_perturbation_size =0.00216982479095459 
 all_img_lst = []
-for i in range(num_samples):
-    for k, (x, x_class) in enumerate(val_loader):
-        samp_img = x[0:1]
-        samp_class = x_class[0].item()
-        samp_img = F.resize(samp_img, [128,128])
-    # Generate the non-perturbed image
-    _, _, img_enc = vae_model.encoder(samp_img.to(device))
-    # decoder
-    original_image =vae_model.decoder(img_enc).detach()
-    original_image1 = F.resize(original_image, [256, 256])
-    original_logit = classifier(original_image1).squeeze().detach().cpu().numpy()
+saved_images = 0  # Counter for saved images
+### Multi-image sample loop ###
+for img_idx in trange(imgs_to_samp):  # Loop runs for 100 iterations
+    # Load the image and class inside the loop
+    for i, (x, _) in enumerate(test_loader):
+        # Move image to device
+        samp_img = x.to(device)
+
+    # VAE encoding and decoding
+    mean, logvar = vae.encode(samp_img)
+    img_enc = vae.reparamatrize(mean, torch.exp(0.5 * logvar))
+    original_lv = img_enc
+    original_image = vae.decode(original_lv)
+
+    # Get the logits and label from classifier
+    original_logit = classifier(original_image).squeeze().detach().cpu().numpy()
     original_label = original_logit.argmax().item()
-    # Assuming original_image is a single image tensor, add batch dimension if not present
-    if original_image.dim() == 3:
-            original_image = original_image.unsqueeze(0)
-   
-    # filename format to save image
-    file_path = os.path.join(result_dir, f'org_label_{original_label}_{i}.png')
 
-    # Save the image
-    save_image(original_image, file_path)
-    # Calculate fitness for the original image
-    fitness_original = calculate_fitness(original_logit, original_label)
-    all_img_lst = []
-
-    ### Initialize optimization ###
-    init_pop = [
-        img_enc + initial_perturbation_size * torch.randn(1, 1024).to(device)
-        for _ in range(pop_size)
-    ]
-    binom_sampler = torch.distributions.binomial.Binomial(
-        probs=0.5 * torch.ones(img_enc.size())
-    )
-    now_pop = init_pop
-    prev_best = np.inf
-    ### GA ###
-    for g_idx in range(gen_num):
-        indivs = torch.cat(now_pop, dim=0)
-        dec_imgs = decoder(indivs)
-       # decoded_img_resized 
-        dec_imgs1 = torch.stack([F.resize(img, [256, 256]) for img in dec_imgs])
-        all_logits = classifier(dec_imgs1).squeeze().detach().cpu().numpy()
-
-        fitness_scores = [
-            calculate_fitness(all_logits[k_idx], original_label)
-            for k_idx in range(pop_size)
+    # Check if the label matches the expected class
+    if original_label == Expected_class_index:
+        # Save the image if it matches
+        save_image(original_image, os.path.join(img_pil_dir, f'original_image_{saved_images}_X{original_label}.png'))
+        saved_images +=1
+        # Process the image with Genetic Algorithm
+        ### Initialize optimization ###
+        init_pop = [
+            original_lv + initial_perturbation_size * torch.randn_like(original_lv).to(device)
+            for _ in range(pop_size)
         ]
 
-        # Perform selection
-        best_idxs = sorted(
-            range(len(fitness_scores)),
-            key=lambda i_x: fitness_scores[i_x],
-            reverse=True,
-        )[-best_left:]
-        # Consider the lowest fitness score
-        now_best = np.min(fitness_scores)
-        parent_pop = [now_pop[idx] for idx in best_idxs]
+        now_pop = init_pop
+        prev_best = np.inf
+        best_fitness_score = np.inf
+        best_image_tensor = None
+        best_image_index = -1
 
-        # Perform crossover and mutation
-        print(
-            "now_best "
-            + str(now_best)
-            + ", Average_score "
-            + str(np.mean(fitness_scores))
+        ### GA ###
+        for g_idx in range(gen_num):
+            indivs = torch.cat(now_pop, dim=0)
+            dec_imgs = vae.decode(indivs)
+            all_logits = classifier(dec_imgs).squeeze().detach().cpu().numpy()
+
+            fitness_scores = [
+                calculate_fitness(all_logits[k_idx], original_label)
+                for k_idx in range(pop_size)
+            ]
+
+            # Finding the minimum fitness score in this generation
+            current_min_index = np.argmin(fitness_scores)
+            current_min_fitness = fitness_scores[current_min_index]
+
+            # Update global minimum if the current score is lower
+            if current_min_fitness < best_fitness_score:
+                best_fitness_score = current_min_fitness
+                best_image_tensor = dec_imgs[current_min_index]
+                best_image_index = current_min_index
+
+            # Perform selection
+            best_idxs = sorted(
+                range(len(fitness_scores)),
+                key=lambda i_x: fitness_scores[i_x],
+                reverse=True,
+            )[-best_left:]
+
+            # Consider the lowest fitness score
+            now_best = np.min(fitness_scores)
+            parent_pop = [now_pop[idx] for idx in best_idxs]
+
+            # Perform crossover and mutation
+            print(
+                "now_best "
+                + str(now_best)
+                + ", Average_score "
+                + str(np.mean(fitness_scores))
+            )
+
+            if now_best < 0:
+                break
+            elif now_best == prev_best:
+                perturbation_size *= 2
+            else:
+                perturbation_size = initial_perturbation_size
+
+            k_pop = []
+            for k_idx in range(pop_size - best_left):
+                mom_idx, pop_idx = np.random.choice(best_left, size=2, replace=False)
+                spl_idx = np.random.choice(512, size=1)[0]
+                k_gene = torch.cat(
+                    [parent_pop[mom_idx][:, :spl_idx], parent_pop[pop_idx][:, spl_idx:]],
+                    dim=1,
+                )  # crossover
+
+                # Mutation
+                diffs = (k_gene != original_lv).float()
+                k_gene += (
+                    perturbation_size * torch.randn(k_gene.size()).to(device) * diffs
+                )  # random adding noise only to diff places
+                k_pop.append(k_gene)
+
+            # Current population is obtained combining the parent pop and its offspring
+            now_pop = parent_pop + k_pop
+            prev_best = now_best
+            now_pop = [torch.clamp(tensor, min=min_val, max=max_val) for tensor in now_pop]
+
+        # Assuming best_image_tensor is tensor that needs to be processed
+        mod_best_image_tensor = best_image_tensor.to(device)  # Move tensor to the appropriate device if not already
+        mod_best_image_np = best_image_tensor.cpu().detach().numpy()  # Convert tensor to numpy array after moving to CPU
+
+        # Assuming classifier is already defined and appropriate for the tensor as is
+        final_bound_logits = classifier(mod_best_image_tensor.unsqueeze(0))  # Ensure tensor is in correct shape for classifier
+        predicted_best_label = torch.argmax(final_bound_logits, dim=1).item()
+
+        # Define the path for saving the numpy file with detailed filename
+        image_path_np = os.path.join(
+            result_dir,
+            f"image_{saved_images}_iteration{g_idx + 1}_X{original_label}_Y{predicted_best_label}.npy"
         )
-        # Log fitness scores
-        # wandb.log({"Fitness Score": now_best, "Sample Index": i, "Iteration": g_idx})
-        if now_best < 0:
-            break
-        elif now_best == prev_best:
-            perturbation_size *= 2
-        else:
-            perturbation_size = initial_perturbation_size
 
-        k_pop = []
-        for k_idx in range(pop_size - best_left):
-            mom_idx, pop_idx = np.random.choice(best_left, size=2, replace=False)
-            spl_idx = np.random.choice(1024, size=1)[0]
-            k_gene = torch.cat(
-                [parent_pop[mom_idx][:, :spl_idx], parent_pop[pop_idx][:, spl_idx:]],
-                dim=1,
-            )  # crossover
+        # Save the numpy array to a file
+        np.save(image_path_np, mod_best_image_np)
 
-            # Mutation
-            diffs = (k_gene != img_enc).float()
-            k_gene += (
-                perturbation_size * torch.randn(k_gene.size()).to(device) * diffs
-            )  # random adding noise only to diff places
-            # random matching to latent_images[i]
-            interp_mask = binom_sampler.sample()
-            interp_mask = interp_mask.to(device)  # Move interp_mask to the same device as img_enc
+        all_img_lst.append(mod_best_image_np)
 
-            k_gene = interp_mask * img_enc + (1 - interp_mask) * k_gene
-            k_pop.append(k_gene)
+        save_image(mod_best_image_tensor, os.path.join(prtb_img_dir, f'image_{saved_images}_Iteration{g_idx + 1}_X{original_label}_Y{predicted_best_label}.png'))
 
-        # Current population is obtained combining the parent pop and its offspring
-        now_pop = parent_pop + k_pop
-        prev_best = now_best
+    else:
+        # If no match is found
+        print(f"No match for image {img_idx}: True class {Expected_class_index}, Predicted class {original_label}")
 
-    mod_best = parent_pop[-1].clone()
-    final_bound_img =vae_model.decoder(parent_pop[-1]).detach().cpu().numpy()
-    all_img_lst.append(final_bound_img)
-    # Convert the image to a PyTorch tensor
-    final_bound_img_tensor = torch.from_numpy(final_bound_img).float().to(device)
-   # final_bound_img_tensor = F.interpolate(final_bound_img_tensor, size=(224, 224), mode='bicubic', align_corners=False)
-
-   # final_bound_img_tensor =F.resize(final_bound_img_tensor, [128, 128])
-    final_bound_img_tensor1 =F.resize(final_bound_img_tensor, [256, 256])
-    prediction = torch.argmax(classifier(final_bound_img_tensor1)).item()
-    predictions.append(prediction)
-    save_image(
-        final_bound_img_tensor,
-        os.path.join(result_dir, f"image_{i}_X{original_label}_Y{prediction}.png"),
-    )
-    # Store the image info
-    image_info.append((i, original_label, prediction))
-
-# Save all generated images as a numpy array
+# Save the images as a numpy array
 all_imgs = np.vstack(all_img_lst)
-np.save("generated_images_vae_Imagenet.npy", all_imgs)
-# Save the image info
-with open(os.path.join(result_dir, "image_info.txt"), "w") as f:
-    f.write("Image Index, Expected Label X, Predicted Label Y\n")
-    for img_info in image_info:
-        f.write(f"{img_info[0]}, {img_info[1]}, {img_info[2]}\n")
-misclassified_count = 0
+np.save(os.path.join(result_dir, "bound_imgs_imgnet_vae.npy"), all_imgs)
 
-# Iterate over the image info list
-for img_info in image_info:
-    expected_label = img_info[1]
-    predicted_label = img_info[2]
-    if predicted_label != expected_label:
-        misclassified_count += 1
-
-misclassification_percentage = (misclassified_count / len(image_info)) * 100
-
-print(f"Misclassification Percentage: {misclassification_percentage:.2f}%")
+# Store the image info
+#image_info.append((next_save_index, g_idx + 1, original_label, predicted_best_label))
