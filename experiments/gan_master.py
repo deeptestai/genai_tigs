4a38e361b5f3a36e67eb5787efe468a4ee4032d2
Maryam <maryamwaheed272@gmail.com>
2024-09-30 12:09:56 +0200

    Update gan_master.py

--- experiments/gan_master.py
+++ experiments/gan_master.py
@@ -4,9 +4,9 @@ import subprocess
 
 def main(dataset):
     dataset_paths = {
-        'mnist': 'mnist_experiments/mnist_gan/cdcgan/sinvad_cdcgan_mnist.py',
-        'svhn': 'svhn_experiments/svhn_gan/cdcgan/sinvad_cdcgan_svhn.py',
-        'cifar10': 'cifar10_experiments/cifar10_gan/cdcgan/sinvad_cdcgan_cifar10.py',
+        'mnist': 'mnist_experiments/mnist_gan/cdcgan/tig_cdcgan_mnist.py',
+        'svhn': 'svhn_experiments/svhn_gan/cdcgan/tig_cdcgan_svhn.py',
+        'cifar10': 'cifar10_experiments/cifar10_gan/cdcgan/tig_cdcgan_cifar10.py',
     }
 
     if dataset not in dataset_paths:
@@ -24,7 +24,7 @@ def main(dataset):
     subprocess.run(['python', script_path])
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run a SINVAD CDCGAN model on a specified dataset.')
+    parser = argparse.ArgumentParser(description='Run a TIG CDCGAN model on a specified dataset.')
     parser.add_argument('--dataset', type=str, required=True, help="Dataset to use. Options: 'mnist', 'svhn', 'cifar10'")
     args = parser.parse_args()
     
